da9cc2617ef10b5d8e94dfd2bf93691429955dba
gkoushik03 <117655611+gkoushik03@users.noreply.github.com>
2023-10-28 14:48:13 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function validateLogin() {
     var username = document.getElementById("username").value;
     if (username.length > 10 && username.includes("@") && (username.endsWith(".com") || username.endsWith(".in"))) {
-    window.location.href = 'wadmain.html';
+    window.location.href = 'index.html';
     } else {
     alert("Invalid username format. Please make sure your username is at least 10 characters long, contains '@', and ends with '.com' or '.in'.");
     }
